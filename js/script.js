5b9449685002cefb2ca1b0f64d2ae11897f37347
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-08 17:00:29 +0100

    adding js loop

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
 // Debug
 console.log('JS OK!')
+
+// Loop
+const cells = document.querySelector('#cells');
+for (let squareIndex = 1; squareIndex <= 100; squareIndex++) {
+
+    // Conditions
+    if (squareIndex % 3 == 0 && squareIndex % 5 == 0) {
+        let element = `<div id="cell-${squareIndex}" class="cell">${'FizBuz'}</div>`
+        cells.innerHTML += element;
+        // new bg
+        const elementCell = document.getElementById('cell-' + squareIndex);
+        elementCell.style.backgroundColor = `red`;
+    } else if (squareIndex % 3 == 0) {
+        let element = `<div id="cell-${squareIndex}" class="cell">${'Fizz'}</div>`
+        cells.innerHTML += element;
+        // new bg
+        const elementCell = document.getElementById('cell-' + squareIndex);
+        elementCell.style.backgroundColor = `orange`;
+    } else if (squareIndex % 5 == 0) {
+        let element = `<div id="cell-${squareIndex}" class="cell">${'Buzz'}</div>`
+        cells.innerHTML += element;
+        // new bg
+        const elementCell = document.getElementById('cell-' + squareIndex);
+        elementCell.style.backgroundColor = `black`;
+    } else {
+        let element = `<div id="cell-${squareIndex}" class="cell">${squareIndex}</div>`
+        cells.innerHTML += element;
+    }
+
+}
+
