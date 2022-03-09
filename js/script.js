9898434bc6c9a6e5360a707f298209d7ab2d3260
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-09 10:42:56 +0100

    indent fix

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ const cells = document.querySelector('#cells');
 for (let squareIndex = 1; squareIndex <= 100; squareIndex++) {
 
     // Conditions
-    if (squareIndex % 3 == 0 && squareIndex % 5 == 0) {
+    if (squareIndex % 3 === 0 && squareIndex % 5 === 0) {
         let element = `<div id="cell-${squareIndex}" class="cell">${'FizBuz'}</div>`
         cells.innerHTML += element;
         // new bg
         const elementCell = document.getElementById('cell-' + squareIndex);
         elementCell.style.backgroundColor = `red`;
-    } else if (squareIndex % 3 == 0) {
+    } else if (squareIndex % 3 === 0) {
         let element = `<div id="cell-${squareIndex}" class="cell">${'Fizz'}</div>`
         cells.innerHTML += element;
         // new bg
         const elementCell = document.getElementById('cell-' + squareIndex);
         elementCell.style.backgroundColor = `orange`;
-    } else if (squareIndex % 5 == 0) {
+    } else if (squareIndex % 5 === 0) {
         let element = `<div id="cell-${squareIndex}" class="cell">${'Buzz'}</div>`
         cells.innerHTML += element;
         // new bg
